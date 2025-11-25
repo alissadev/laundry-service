54359e8e06bbe4d69d8ada232aa30b2ab8b1b4b6
Alissa Troiano <alissatroianonyc@gmail.com>
2025-11-25 13:21:07 -0500

    Update heading with logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Services } from './components/Services';
 import { Features } from './components/Features';
 import { HowItWorks } from './components/HowItWorks';
 import { Pricing } from './components/Pricing';
-import { Reviews } from './components/Reviews';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import { Reviews } from './components/UserReviews';
